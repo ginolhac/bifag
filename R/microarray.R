f58536bc82ac174560ac634ab5a1cdbca5a0d092
Aurelien Ginolhac <ginolhac@gmail.com>
2017-02-24 10:22:53 +0100

    NSE for dynamic column specifications

--- R/microarray.R
+++ R/microarray.R
@@ -1,27 +1,54 @@
 #' @import dplyr
+#' @importFrom lazyeval interp
+#' @importFrom stats setNames
 NULL
 
+#' Summarised gene symbols by probeset
+#'
 #' Append probeset ids when more than 1 for gene symbol
-#' @param seg data.frame of cnv
-#' @param column column name for patients
-#' @param patient string of patient to keep
-#' @param ref data.frame of genes
-#' @return A data.frame of assigned cnvs
+#' the returned data frame will have unique gene symbols present only once
+#' @param df data.frame or tibble
+#' @param gene column name for gene symbols
+#' @param probe column name for probeset_id
+#' @return A data.frame of summarised gene symbols
 #' @export
 #' @examples
 #' \dontrun{
 #' library(bifag)
 #'
-#' res <- append_multiprobe_genes(my_df)
+#' res <- append_multiprobe_genes(df)
 #' }
 #'
 
-append_multiprobe_genes <- function(df) {
+append_multiprobe_genes <- function(df, gene = "gene_symbol", probe = "probeset_id") {
+  # Check whether data.frame name is supplied
+  if (is.null(df)) {
+    stop("data frame not supplied")
+  }
+  if (gene %in% colnames(df) == FALSE) {
+    stop(paste(gene, "must be a column in the specified dataframe"), call. = FALSE)
+  }
 
+  if (probe %in% colnames(df) == FALSE) {
+    stop(paste(probe, "must be a column in the specified dataframe"), call. = FALSE)
+  }
+  # columns to keep in the end, drop n and probeset
+  keep <- names(df)[!names(df) %in% c(probe, "n")]
+  # mix od constant and variables, better use lazyeval with
+  # - n is from the data frame so we quote it
+  # gene comes is local variable from formals
+  mut_call <- lazyeval::interp(~if_else(nb > 1, paste(gs, pbs, sep = "_"), gs),
+                               nb = quote(n),
+                               gs = as.name(gene),
+                               pbs = as.name(probe))
   df %>%
-    group_by(gene_symbol) %>%
+    group_by_(gene) %>%
     mutate(n = n()) %>%
     ungroup() %>%
-    mutate(gene_symbol = if_else(n > 1, paste(gene_symbol, probeset_id, sep = "_"), gene_symbol)) %>%
-    select(-probeset_id, -n)
+    # overwrite the gene symbol column, use the interp as name and setNames to get
+    # the dynamic col name right
+    mutate_(.dots = setNames(list(mut_call), gene)) %>%
+    select_(.dots = keep)
+  # alternative for negative selection
+  # select_(lazyeval::interp(~-prob, prob = as.name(probe)))
 }
--- tests/testthat/test-microarray.R
+++ tests/testthat/test-microarray.R
@@ -7,3 +7,11 @@ test_that("append probeset ids", {
   res <- append_multiprobe_genes(toptable)
   expect_equal(res$gene_symbol, c("Myo_1101", "Zpt", "Myo_1103"))
 })
+
+test_that("append probeset ids, col names", {
+  toptable2 <- data.frame(my_gene = c("Myo", "Zpt", "Myo"),
+                         my_probe = c("1101", "1102", "1103"),
+                         stringsAsFactors = FALSE)
+  res2 <- append_multiprobe_genes(toptable2, gene = "my_gene", probe = "my_probe")
+  expect_equal(res2$my_gene, c("Myo_1101", "Zpt", "Myo_1103"))
+})
