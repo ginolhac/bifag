84c14f2dbb97c7ff2bf728083790995900ce6c06
Aurelien Ginolhac <ginolhac@gmail.com>
2017-02-24 11:01:13 +0100

    need toptable and newline

--- tests/testthat/test-microarray.R
+++ tests/testthat/test-microarray.R
@@ -16,8 +16,10 @@ test_that("append probeset ids, col names", {
   expect_equal(res2$my_gene, c("Myo_1101", "Zpt", "Myo_1103"))
 })
 
-
 test_that("test errors on invalid inputs", {
+  toptable <- data.frame(gene_symbol = c("Myo", "Zpt", "Myo"),
+                         probeset_id = c("1101", "1102", "1103"),
+                         stringsAsFactors = FALSE)
   expect_error(append_multiprobe_genes(df = NULL), "`df` must be a data frame / tibble")
   expect_error(append_multiprobe_genes(df = c(1:3)), "`df` must be a data frame / tibble")
   expect_error(append_multiprobe_genes(toptable, gene = "zzz"), "zzz must be a column in the specified dataframe")
