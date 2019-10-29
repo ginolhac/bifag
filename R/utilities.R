6c40557ac8b51476f0352b584e01ad956df9ce4c
Aurelien Ginolhac <ginolhac@gmail.com>
2019-10-29 09:37:01 +0100

    bugfix string

--- R/utilities.R
+++ R/utilities.R
@@ -16,7 +16,7 @@ NULL
 #' @export
 cluster_mount <- function(cluster) {
   test_bashrc <- file.path(Sys.getenv("HOME"), cluster, ".bashrc")
-  if (!file.exists("test_bashrc")) {
+  if (!file.exists(test_bashrc)) {
     message(paste(cluster, "not mounted, mounting..."))
     mnt <- paste0(cluster, "_mount")
     stopifnot(file.exists(file.path(Sys.getenv("HOME"), "bin", mnt)))
